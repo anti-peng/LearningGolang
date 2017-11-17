6bbcfeca0d4661bc7de0b4dafa6d932a44bc5d32
fry <anti.peng@gmail.com>
2017-11-17 18:05:40 +0800

    more io

--- toy2/t4_seeker_closer.go
+++ toy2/t4_seeker_closer.go
@@ -2,9 +2,12 @@ package toy2
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
+	"io"
 	"os"
 	"strings"
+	"time"
 )
 
 // Seek 设置下一次 Read / Write 的偏移量为 offset
@@ -107,3 +110,89 @@ func demoByteScanner() {
 	readOneByte(buffer)
 	readOneByte(buffer)
 }
+
+// SectionReader
+// 一个 struct 没有任何导出字段 实现了 Read Seek 和 ReadAt
+// 内嵌 ReaderAt
+// type SectionReader struct {
+// 	r     ReaderAt
+// 	base  int64
+// 	off   int64
+// 	limit int64
+// }
+
+// LimitedReader
+// 从 R 读取但将返回数据量限制为 N 个字节
+// type LimitedReader struct {
+// 	R Reader
+// 	N int64
+// }
+
+// package io 中，LimitReader 的实现是调用 LimitedReader
+// func LimitReader(r Reader, n int64) Reader {
+// 	return &LimitedReader{r, n}
+// }
+func demoLimitReader() {
+	reader := strings.NewReader("this is an LimitReader Example")
+	limitReader := &io.LimitedReader{R: reader, N: 8}
+	for limitReader.N > 0 {
+		tmp := make([]byte, 2)
+		limitReader.Read(tmp)
+		fmt.Printf("%s", tmp)
+	}
+	fmt.Print("\n")
+}
+
+// PipeReader 和 PipeWriter
+// implements interface io.Reader & io.Closer
+
+// Read - get data from channel, will block until 写入端开始写入数据
+// 或者写入端关闭了。如果写入端关闭时带上error（call CloseWithError) 该
+// 方法返回的 err 就是写入端传递的error，否则 err 为 EOF
+
+// Write - 写数据到管道中 会堵塞直到管道读取端读完所有数据或者读取端关闭了。
+// 读取端错误 ErrClosedPipe / 读取端传递的error
+
+func demoPipeWrite(pw *io.PipeWriter) {
+	var (
+		i   = 0
+		err error
+		n   int
+	)
+
+	data := []byte("你好 golang")
+
+	for n, err = pw.Write(data); err == nil; _, err = pw.Write(data) {
+		i++
+		if i == 3 {
+			pw.CloseWithError(errors.New("输出3次后结束"))
+		}
+	}
+	fmt.Println("close 后输出的字节数： ", n, " error: ", err)
+}
+
+func demoPipeReader(pr *io.PipeReader) {
+	var (
+		err error
+		n   int
+	)
+
+	data := make([]byte, 1024)
+
+	for n, err = pr.Read(data); err == nil; _, err = pr.Read(data) {
+		fmt.Printf("%s\n", data[:n])
+	}
+	fmt.Println("writer closeWithError: ", err)
+}
+
+func demoPipe() {
+	pr, pw := io.Pipe()
+	go demoPipeWrite(pw)
+	go demoPipeReader(pr)
+	time.Sleep(time.Second * 5)
+}
+
+// Copy & CopyN
+// func Copy(dst Writer, src Reader) (written int64, err error)
+// copy src -> dst, until src get EOF or error;
+// copy 从 src 读取直到 EOF为止
