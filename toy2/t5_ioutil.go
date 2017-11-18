2a16ba6c6359ec567e5bdf421c53bbc020193bb1
fry <anti.peng@gmail.com>
2017-11-18 18:17:45 +0800

    fmt @#%3591#%^$%^&$%^&%$^

---
+++ toy2/t5_ioutil.go
@@ -0,0 +1,14 @@
+package toy2
+
+// NopCloser
+// ReadAll
+// ReadDir
+// ReadFile WriteFile
+// TempDir TempFile
+// defer func() {
+// 	f.Close()
+// 	os.Remove(f.Name())
+// }()
+
+// Discard
+// type devNull int
